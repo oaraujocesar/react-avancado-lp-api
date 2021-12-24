86f74a83d85dacd72c5a99480e61d4d0ee666452
César O. Araújo <oaraujocesar@gmail.com>
2021-12-24 14:01:25 -0300

    Add missing middlewares

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,4 +1,12 @@
 module.exports = [
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::favicon',
+  'strapi::public',
   {
     name: 'strapi::security',
     config: {
